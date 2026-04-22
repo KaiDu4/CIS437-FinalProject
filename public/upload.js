08532d53c85b01d9226260d76a9e32ad118c49ed
kai <dulakkai@gmail.com>
2026-04-21 23:57:21 -0400

    upload.html/js

---
+++ public/upload.js
@@ -0,0 +1,23 @@
+import {initializeApp} from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js';
+import {getStorage, ref} from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-storage.js';
+// Web app Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDZytRK5UceQcWV-Y5RDxmd1Lq3iKx4yrI",
+    authDomain: "dulak-finalproject-cis437.firebaseapp.com",
+    projectId: "dulak-finalproject-cis437",
+    storageBucket: "dulak-finalproject-cis437.firebasestorage.app",
+    messagingSenderId: "229405261161",
+    appId: "1:229405261161:web:9ed022cdb9f0c1406611f8"
+};
+
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+
+document.getElementById("submit").addEventListener("click", function (event) {
+    event.preventDefault();
+    const author =  document.getElementById("name").value;
+    const title = document.getElementById("title").value;
+    const date = new Date().toDateString();
+    const file = document.getElementById("file").files[0];
+})
+
